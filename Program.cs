a464b7c92afcbcaeb7e5c952a321a9ba3a4922ee
João Paulo <167019869+devjoaolimaw@users.noreply.github.com>
2024-06-07 16:28:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿(int, string, string, decimal) tupla = (1, "Joao", "Lima", 1.80M);
+
+ValueTuple<int, string, string, decimal> outroExemploTupla = (1, "Joao", "Lima", 1.80M);
+// // var outroExemploTuplaCreate = Tuple.Create(1, "Joao", "Lima", 1.80M);
+
+Console.WriteLine($"Id: {tupla.Item1}");
+Console.WriteLine($"Nome: {tupla.Item2}");
+Console.WriteLine($"Sobrenome: {tupla.Item3}");
+Console.WriteLine($"Altura: {tupla.Item4}");
